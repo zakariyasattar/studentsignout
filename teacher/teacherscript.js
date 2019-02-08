05653497b04efc3ab330df2c265c00826452ea81
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-02-07 22:32:29 -0600

    uncomment auth

--- teacher/teacherscript.js
+++ teacher/teacherscript.js
@@ -1,37 +1,37 @@
-// document.getElementById('html').style.display = "none";
-//
-// var provider = new firebase.auth.GoogleAuthProvider();
-//
-// firebase.auth().signInWithPopup(provider).then(function (result) {
-//   // This gives you a Google Access Token. You can use it to access the Google API.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//
-//   uid = user.uid;
-//   if (uid === "qaOIHfkRUrVlMVKxkfDukSm6k5l1"){
-//     document.getElementById('html').style.display = "initial";
-//   }
-//
-//   // ...
-//
-// }).catch(function (error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-//   console.log("Authentication Error");
-// });
-//
-// firebase.auth().onAuthStateChanged(function (user){
-//   if (user) {
-//     console.log("current user uid = " + user.uid);
-//   }
-// });
+document.getElementById('html').style.display = "none";
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function (result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+
+  uid = user.uid;
+  if (uid === "qaOIHfkRUrVlMVKxkfDukSm6k5l1"){
+    document.getElementById('html').style.display = "initial";
+  }
+
+  // ...
+
+}).catch(function (error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+  console.log("Authentication Error");
+});
+
+firebase.auth().onAuthStateChanged(function (user){
+  if (user) {
+    console.log("current user uid = " + user.uid);
+  }
+});
 
 console.clear();
 

3c7bf484c38ddf747374f68e885263542ebfeff4
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-09-18 18:56:10 -0500

    implemented new teacher data

--- teacher/teacherscript.js
+++ teacher/teacherscript.js
@@ -2,210 +2,175 @@ document.getElementById('html').style.display = "none";
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
-firebase.auth().signInWithPopup(provider).then(function (result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
+if(localStorage.getItem('uid') === "qaOIHfkRUrVlMVKxkfDukSm6k5l1" || localStorage.getItem('uid') === "oIe1Ej0XetZycy6rq9zFY6euz6q2") {
+  document.getElementById('html').style.display = "initial";
+}
+else {
+  firebase.auth().signInWithPopup(provider).then(function (result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
 
-  uid = user.uid;
-  if (uid === "qaOIHfkRUrVlMVKxkfDukSm6k5l1" || uid === "oIe1Ej0XetZycy6rq9zFY6euz6q2"){
-    document.getElementById('html').style.display = "initial";
-  }
+    uid = user.uid;
+    if (uid === "qaOIHfkRUrVlMVKxkfDukSm6k5l1" || uid === "oIe1Ej0XetZycy6rq9zFY6euz6q2"){
+      document.getElementById('html').style.display = "initial";
+      localStorage.setItem('uid', uid);
+    }
 
-  else {
-    document.write("NOT AUTHORIZED");
-  }
+    else {
+      document.write("NOT AUTHORIZED");
+    }
 
 
-}).catch(function (error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
 
-  console.log("Authentication Error");
-});
-
+    alert("Authentication Error");
+  });
+}
 console.clear();
 
-function getClasses(){
-  var classes = [];
-  var formData = new FormData();
-  formData.append("teacherFirst", "Kathleen M");
-  formData.append("teacherLast", "Buttitta");
-
-  var request = new XMLHttpRequest();
-  request.open("POST", "http://fahrenbacher.com/groups/classes.php");
-  request.onreadystatechange = function ()
-  {
-      if(request.readyState === 4)
-      {
-        if(request.status === 200 || request.status === 0)
-        {
-          var result = request.responseText;
-
-          var cs = result.split("\n");
-          for(var i = 0; i < cs.length - 1; i++) {
-              classes.push(cs[i].split(","));
-          }
-      }
-    }
-  }
-  request.send(formData);
-  return classes;
-}
-
-function getStudents(period) {
-    var formData = new FormData();
-    var students = [];
-    formData.append("teacherFirst", "Kathleen M");
-    formData.append("teacherLast", "Buttitta");
-    formData.append("period", period);
-
-    var request = new XMLHttpRequest();
-    request.open("POST", "http://fahrenbacher.com/groups/students.php");
-    request.onreadystatechange = function ()
-    {
-        if(request.readyState === 4)
-        {
-            if(request.status === 200 || request.status === 0)
-            {
-                //the result String will be in the following format:
-                //  <student id>,<student first name>,<student last name>,<student email>\n
-                var result = request.responseText;
-
-                var ss = result.split("\n");
-                for(var i = 0; i < ss.length - 1; i++) { //last line has no data
-                    var stud = ss[i].split(",");
-                    students.push(stud);
-                }
-            }
-        }
-    }
-    request.send(formData);
-    return students;
-  }
-
 classes = [
-  [ 'alg6',
-    'Adrian Murillo:67305',
-    'Josemari Deniega:67536',
-    'Alexander Ethan Merkel:67625',
-    'Tiara Banac Puente:68128',
-    'Maryam Al-Salihi:68284',
-    'Alec Ulbert:71233',
-    'Nicole Piro:72196',
-    'NICOLE ODISHO:72838',
-    'BRYAN TRAN:72863',
-    'Khanh Huynh:72906',
-    'Mohammed Siddiqui:72921',
-    'Izabella Putrus:73007',
-    'Saad Iqbal:73381'
+  [ 'geoext3',
+  '65893:Michelle Herrarte',
+  '67536:Josemari Deniega',
+  '67626:Matthew Jones',
+  '68155:Jocelyn Aguilar',
+  '72098:Maximis Puente',
+  '72838:NICOLE ODISHO',
+  '72951:Jaiden Concepcion',
+  '73393:Anya Siddiq'
      ],
-  [ 'geo1',
-    'Oscar Vega:65594',
-    'Aida Mehicevic:65764',
-    'Erick Mora Vargas:65894',
-    'Joshua Padios:66171',
-    'Baha Mohamad:67226',
-    'Lexie Bugara:67415',
-    'Noor Anwar:67836',
-    'Rami Lahluh:67841',
-    'Alexandru Craciun:67920',
-    'Chloe Jao:67960',
-    'Janay Moncrief:68131',
-    'Rabia Khan:69339',
-    'Angelyn Cagatan:71251',
-    'Katlyn Arnold:71908',
-    'Ashour Daniel:71914',
-    'Krystian Gadek:71920',
-    'John Jado:71927',
-    'Cameron Johnson:71929',
-    'Andrew Zadesenets:71969',
-    'Ariana Murphy:72085',
-    'Olivia Furlano:72145',
-    'Nicolas Vera:72230',
-    'Gael Gutierrez:72321',
-    'Brandon Ayala:72583',
-    'Jake Hoffman:72614',
-    'Lauren Rubio:72651',
-    'Patrick Busch:73458'
+  [ 'HR',
+  '65761:Nicholas Ortiz',
+  '66472:Veronica Grden',
+  '67044:Kayla Claybron',
+  '67581:Zachary Thompson',
+  '67595:Ella Winston',
+  '67630:Ali Akram',
+  '67743:Isabella Roque',
+  '68169:Hayden Ferrigno',
+  '70651:Damian Kranski',
+  '70670:Boris Tiongson',
+  '70764:Davy Khim',
+  '70916:Jayden Besoaga',
+  '70971:Vincent Mangano',
+  '70972:Ruby Martinez',
+  '70998:Abid Rahman',
+  '71151:Kadin Camburn',
+  '71234:Antoinette Vandenbroucke',
+  '71266:Jibran Khandwala',
+  '71319:Andrew Zomaya',
     ],
-  [ 'geo2',
-    'Enesa Dibra:65656',
-    'Omar Hussaini:66891',
-    'Tairim Pretha:67805',
-    'Burhan Bhatti:67896',
-    'Hajirah Khan:68056',
-    'Ashley Youkhana:68599',
-    'Monique McDonald:68603',
-    'Hoang Vo:68829',
-    'Isaiah Liboy:68848',
-    'Daniel Belay:69168',
-    'Mujeeb Ansari:69340',
-    'Zehra Rizvi:70791',
-    'Jason Smalbergher:71015',
-    'Lilian Hwang:71059',
-    'Amanze Nwosu:71207',
-    'Ayman Jaddi:71926',
-    'Paul Karabatsos:71930',
-    'Janelle Rodriguez:71951',
-    'George Rosca:71952',
-    'Ronnie Leavitt:72175',
-    'Aldin Perazic:72194',
-    'Angelina Sikaras:72214',
-    'Maya Rios:72648'
+  [ 'precaladv5',
+  '65542:Dulguun Bat-Erdene',
+  '65733:Esha Shah',
+  '66155:Phil Myco Zulueta',
+  '66660:Amanda Hong',
+  '67196:Hajer Alasali',
+  '68020:Umaimah Ansari',
+  '72050:Clarissa Curescu',
+  '72083:Eden Melamed',
+  '72117:Daniel Arnautu',
+  '72137:Andjela Dimitrijevic',
+  '72143:Joaquin Felix Gomez',
+  '72154:Lauren Hersh',
+  '72586:Emma Beavers',
+  '72606:Harsi Gandhi',
+  '72694:Oliver Kozub',
+  '72702:Thomas Magdaleno',
+  '72715:Nandini Shah',
+  '72722:Sharon Varghese',
+  '73464:Omar Louafi'
      ],
-  [ 'ps5',
-    'Susan Giffen:62570',
-    'Kayla Lawrence:62608',
-    'Christian Dangelo Lewis:62610',
-    'Hevan Patel:62648',
-    'Sloan Tomczak:62684',
-    'Stefan Tranescu:62687',
-    'Steven Agos:63080',
-    'Umaimah Siddiqui:63204',
-    'Stephanie Sorisho:63208',
-    'Divya Patel:63272',
-    'Aleksandra Smyk:63404',
-    'Melissa Alanis:63414',
-    'Christian Garcia:63440',
-    'Maha Shakir:63498',
-    'Samantha Simkins:63499',
-    'Nabeel Qadri:68582',
-    'Samuel Montgomery:71202'
+  [ 'geo6',
+  '65893:Michelle Herrarte',
+  '66022:Benipal Nano',
+  '67600:Adelaila Morales',
+  '67782:Israel Islas Diaz',
+  '68260:David Leuviah Ortinero',
+  '68355:Zaya Awriany',
+  '69394:Jaisha Tariq',
+  '70701:Aman Syed',
+  '70745:Rida Rehman',
+  '71299:Tahniyath Syeda',
+  '72099:Faris Rahic',
+  '72234:Nareg Yacoubian',
+  '72807:BRANDON GAODE',
+  '72812:JENNIFER GUERRERO',
+  '72878:Sarah Cachila',
+  '72890:Arucia Elahi',
+  '72895:Miki Canak',
+  '72910:Katrin Tamo',
+  '72951:Jaiden Concepcion',
+  '72962:Leah Sada',
+  '72969:Nicholas Zervos',
+  '73377:Matthew Chung',
+  '73456:Leona Younatham'
      ],
-  [ 'ps7',
-    'Branden Logal:62322',
-    'Jane Bochnia:62536',
-    'Aritri Dutta:62558',
-    'Sarah Sundrani:62677',
-    'Taral Talati:62678',
-    'Dylan Chikko:63104',
-    'Evan Han:63134',
-    'Marina Palladinetti:63180',
-    'Isabella Reza:63195',
-    'Maham Syed:63212',
-    'Laura Trinh:63220',
-    'Gabriella Goldman:63245',
-    'Christina Lappas:63261',
-    'Nicole Juszczyk:63371',
-    'Anna Krasiy:63375',
-    'Katarina Letica:63379',
-    'Wyatt Zwik:63411',
-    'Christian Castillo:63424',
-    'Soohyeon Jung:63451',
-    'Rolands Teivans:63507',
-    'Shelly Sabaricos:65487',
-    'Husayn Dashtee:65632',
-    'Angelyna Madrid:66222',
-    'Zehra Rizvi:70791',
-    'Aileen Nguyen:71205'
-     ] ]
+  [ 'precaladv8',
+  '65693:Charlotte Howard-Check',
+  '66511:Fahad Khan',
+  '67358:Faheem Patel',
+  '67958:Dionisios Koukounaras',
+  '68037:Aadil Patel',
+  '68051:Rabeeka Khan',
+  '68815:Ali Nawaz',
+  '69045:Christine Nguyen',
+  '69106:Tyreke Beverly',
+  '71277:Savonna Mosio',
+  '71905:Manal Ali',
+  '71918:Nimra Fatima',
+  '72122:Ephraim Bennett',
+  '72174:Brandon Le',
+  '72201:Katerina Rizzotti',
+  '72206:Philip Saba',
+  '72217:Daniella Suh',
+  '72218:Ethan Suh',
+  '72220:Laiba Sukhera',
+  '72226:Harry Truong',
+  '72595:Jeremiah Curescu',
+  '72631:Jessica Lu',
+  '72644:Sophia Nicole Ramos',
+  '73459:Dharma Delahanty'
+  ],
+[ 'geo9',
+  '65499:Katelyn Rivero',
+  '66004:Wilbert Escalante',
+  '67626:Matthew Jones',
+  '67913:Ahad Ali Ahmed',
+  '68155:Jocelyn Aguilar',
+  '68367:Faith Khoshaba',
+  '69301:Nazanin Wahdi',
+  '72067:Jasmine Kaur',
+  '72098:Maximis Puente',
+  '72579:Wasif Amin',
+  '72676:Inara Ali',
+  '72723:Alejandro Villegas',
+  '72838:NICOLE ODISHO',
+  '72840:Mehreen Patel',
+  '72845:Nathan Piuian',
+  '72869:NICHOLAS YONAN',
+  '72872:Elizabeth Irving',
+  '72907:Melisa Gundogdu',
+  '72944:Adan Fatima',
+  '72948:Elias Azami',
+  '72950:Marya Khan',
+  '72985:Emily Suh',
+  '73001:Orama Korel',
+  '73393:Anya Siddiq',
+  '73406:Alexis Rebollar',
+  '73436:Elizabeth Kempf',
+  '75147:Naveen Mallett'
+]
+ ]
 var selectedName = getSelected();
 var totalSeconds = [];
 var data = [];
@@ -265,12 +230,12 @@ firebaseRef.child("signOut").on("value", function(snapshot) {
 });
 
 
-function nameOfStudentWithId(id) {
+function nameOfStudentWithId(id_passed) {
   for(var i = 0; i < classes.length; i++){
     for(var j = 0; j < classes[i].length; j++){
-      var name = classes[i][j].substring(0, classes[i][j].indexOf(":"))
-      var id1 = classes[i][j].slice(classes[i][j].indexOf(":") + 1);
-      if(id == id1){
+      var id = classes[i][j].substring(0, classes[i][j].indexOf(":"))
+      var name = classes[i][j].slice(classes[i][j].indexOf(":") + 1);
+      if(id == id_passed){
         return name;
       }
     }
@@ -298,22 +263,8 @@ function createKey() {
   var counter = 0;
 
   for(var i = 0; i < classes.length; i++){
-    for(var j = 0; j < classes[i].length; j++){
-      if(i == 0){
-        currentClass = "alg6";
-      }
-      if(i == 1){
-        currentClass = "geo1";
-      }
-      if(i == 2){
-        currentClass = "geo2";
-      }
-      if(i == 3){
-        currentClass = "ps5";
-      }
-      if(i == 4){
-        currentClass = "ps7";
-      }
+    for(var j = 1; j < classes[i].length; j++){
+      currentClass = classes[i][0];
       counter++;
       var bg_colour = Math.floor(Math.random() * 16777215).toString(16);
       bg_colour = "#" + ("000000" + bg_colour).slice(-6);
@@ -701,9 +652,6 @@ function getTimeOutId(id){
 
 
 function bootstrap() {
-    var targetDiv = document.getElementById("summary");
-    targetDiv.style.marginBottom = "66px";
-
     populateHeader();
     populateTable();
 
